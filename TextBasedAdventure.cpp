b5e1bf314fa25acd391692e0f0df9b45ae0d778a
Marcus Bean <marcus.bean@outlook.com>
2017-12-07 17:18:21 -0600

    intial project version

---
+++ TextBasedAdventure.cpp
@@ -0,0 +1,12 @@
+#include "TextBasedAdventure.h"
+
+int game()
+{
+	return 0;
+};
+
+int main(int argc, char** argv)
+{
+	cout << "Welcome to Castlestein\n";
+	return 0;
+}
---
+++ TextBasedAdventure.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <iostream>
+using std::cout;
+
+struct Location
